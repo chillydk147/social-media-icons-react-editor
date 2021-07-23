4bec47e70ba66830d3bc5a92f3661044e38399b1
chillydk147 <dmq@outlook.ie>
2021-07-23 16:33:13 +0100

    #5

--- src/App.js
+++ src/App.js
@@ -3,25 +3,35 @@ import './App.css';
 
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { withStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  componentDidMount(){
+    
+  }
+
+  render(){   
+    const { classes } = this.props;
+    return (
+      <div>        
+        abcdef
+      </div>
+    );
+  }
 }
 
 export default App;
