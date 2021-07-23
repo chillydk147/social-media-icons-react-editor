6415700441163d5e4c9beb08fa5f80e50e1970ba
chillydk147 <dmq@outlook.ie>
2021-07-23 17:05:49 +0100

    #10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
 
 class App extends React.Component {
   render(){   
     return (
       <div>        
-        abcde
+        abcde1npm run build
       </div>
     );
   }
