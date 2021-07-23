6ff915a70e498a706f03701e6e45e3eff6d0a376
chillydk147 <dmq@outlook.ie>
2021-07-23 17:01:55 +0100

    #9

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render(){   
     return (
       <div>        
-        abcdef
+        abcde
       </div>
     );
   }
