6d20a960955a95636579c4f8df853fe05be2fe4f
chillydk147 <dmq@outlook.ie>
2021-07-23 16:54:58 +0100

    #8

--- src/App.js
+++ src/App.js
@@ -6,16 +6,9 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
-  }
-  componentDidMount(){
-    
   }
 
   render(){   
-    const { classes } = this.props;
     return (
       <div>        
         abcdef
